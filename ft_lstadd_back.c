b8166093d846c0ba15500c8a5ddb5caef66a5608
worthy1973 <daniel.lopez@personasfelices.com>
2022-11-03 18:03:04 +0100

    Lists Manipulations

---
+++ ft_lstadd_back.c
@@ -0,0 +1,16 @@
+:w
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (lst)
+	{
+		if (!*lst)
+			*lst = new;
+		else
+		{
+			temp = ft_lstlast(*(lst));
+			temp->next = new;
+		}
+	}
+}
